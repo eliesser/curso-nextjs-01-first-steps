64acfc59eb6ada5a2344df8099b2bfd5d5009e65
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-28 16:15:14 +0200

    git commit -m "first commit"

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Pricing Page',
+  description: 'Pricing Page Description',
+  keywords: ['Pricing Page'],
+};
+
+export default function PricingPage() {
+  return (
+    <>
+      <span className='text-7xl'>Pricing Page</span>
+    </>
+  );
+}
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { HomeIcon } from '@primer/octicons-react';
+
+import Link from 'next/link';
+
+import { ActiveLink } from '../active-link/ActiveLink';
+
+const navItems = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+];
+
+export const Navbar = async () => {
+  return (
+    <nav className='flex bg-blue-800 bg-opacity-20 p-2 m-2 rounded'>
+      <Link className='flex items-center gap-2' href='/'>
+        <HomeIcon />
+        Home
+      </Link>
+
+      <div className='flex flex-1'></div>
+
+      {navItems.map((navItem) => (
+        <ActiveLink key={navItem.path} {...navItem} />
+      ))}
+    </nav>
+  );
+};
